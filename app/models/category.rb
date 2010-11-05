1497af0561320b3a5b69eb59c5767a6ae789bc99
Kamil Tusznio <kamil@causes.com>
2010-11-04 19:00:34 -0700

    sanitizing views

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,8 @@
 class Category < ActiveRecord::Base
   has_many :chips
 
+  validates_uniqueness_of :name
+
   def to_s
     name
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   has_many :chips
 
+  validates_uniqueness_of :name
+
   def category_count(category, date = Date.today)
     self.chips.select { |c| c.category == category && c.created_at.getlocal.to_date == date }.size
   end
